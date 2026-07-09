482df075b373a80e204d81ea66926a60fcc878fb
Shubh Shrivastava <shubh.cs28@iilm.edu>
2026-07-09 14:35:20 +0530

    Add files via upload

---
+++ utils/languages.py
@@ -0,0 +1,14 @@
+LANGUAGES = {
+    "English": "en",
+    "Hindi": "hi",
+    "French": "fr",
+    "German": "de",
+    "Spanish": "es",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Chinese": "zh-CN",
+    "Russian": "ru",
+    "Arabic": "ar",
+    "Portuguese": "pt",
+    "Korean": "ko"
+}
